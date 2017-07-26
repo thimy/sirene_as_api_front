13cbbcd64cd0283c6dc396f2d3b6e9592e14b2a4
Zerhariel <sfaure@student.42.fr>
2017-07-26 14:04:03 +0200

    Ajout calcul TVA intracommunautaire

--- src/components/Entreprise.vue
+++ src/components/Entreprise.vue
@@ -1,37 +1,42 @@
 <template>
   <div>
-    <router-link to="/">Revenir aux résultats</router-link>
-    <div v-if="result !== null">
-      <h3>Bienvenue sur la page Entreprise {{$route.params.siret}}</h3>
+    <router-link to=/>Revenir aux résultats</router-link>
+    <div :if="result !== null">
       <!-- debugResults : {{singleResult}} -->
       <p>Nom entreprise : {{result.nom_raison_sociale}}</p>
       <ul>
         <li> Siret : {{result.siret}}</li>
-        <li> Sirene : {{result.siren}}</li>
+        <li> Siren : {{result.siren}}</li>
+        <li> Clef NIC : {{result.nic}}</li>
         <li> Code postal : {{result.code_postal}}</li>
+        <li> Cedex : {{result.cedex}}</li>
         <li> Departement : {{result.departement}}</li>
         <li> Date de création : {{result.date_creation}}</li>
+        <li> Numéro TVA Intracommunautaire : {{tvaIntracommunautaire}}
         <li> Téléphone : {{result.telephone}}</li>
         <li> Email : {{result.email}}</li>
         <li> Code activité principale : {{result.activite_principale_entreprise}}</li>
-        <li> Libéllé activité principale : {{result.libelle_activite_principale_entreprise}}</li>
+        <li> Libellé activité principale : {{result.libelle_activite_principale_entreprise}}</li>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
-/* eslint-disable no-undef */
 export default {
   name: 'Entreprise',
   computed: {
-    result: function () {
+    result() {
       return this.$store.getters.singlePageResultEtablissement
+    },
+    tvaIntracommunautaire() {
+      const tvaKey = (12 + 3 * (this.result.siren % 97)) % 97
+      const tvaNumber = `${tvaKey}${this.result.siren}`
+      return `FR${tvaNumber}`
     }
   },
-  created () {
+  created() {
     this.$store.commit('executeSearchBySiret', this.$route.params.siret)
   }
 }
-
 </script>
