1358dd74a812f1f64a70e5d5a36bdb9e3ee6554f
Thimy Kieu <thimy.nya@gmail.com>
2017-09-05 17:10:01 +0200

    improve: pagination style

--- src/components/paginate/PaginateModule.vue
+++ src/components/paginate/PaginateModule.vue
@@ -37,11 +37,32 @@ export default {
 
 .pagination {
 	list-style: none;
-  display: inline;
+  display: flex;
+  justify-content: flex-end;
 }
 
-li.pagesButtons {
-  display: inline;
+.pagesButtons {
+  background: transparentize($color-light-blue, .8);
+  border-radius: 3px;
+  padding: 2px 5px;
+  margin-left: 8px;
+
+  &.disabled, &:first-child, &:last-child {
+    background: none;
+  }
+
+  &.active {
+    background-color: $color-light-blue;
+
+    a {
+      color: $color-white;
+    }
+  }
+}
+
+.pagesButtons a {
+  color: $link-color;
+  font-weight: 700;
 }
 
 </style>
