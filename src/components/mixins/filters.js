028d38c2ad0435901da38c390b9dba3f2fc08215
Samuel FAURE <samuel.faure@hotmail.com>
2017-09-07 13:24:05 +0200

    Update filters.js

--- src/components/mixins/filters.js
+++ src/components/mixins/filters.js
@@ -1,7 +1,7 @@
 export default {
   filters: {
     capitalize: function (string) {
-      if (string !== null) {
+      if (string) {
         return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
       }
     },
