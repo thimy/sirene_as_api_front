44e4315a4e9e7ac30d47622d1e4d54cd0f3295d8
Samuelfaure <sfaure@student.42.fr>
2017-10-16 14:20:28 +0200

    Cleaner constants in production

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,5 +2,9 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+
+  BASE_ADRESS: '"http://localhost:3000/full_text/"',
+  BASE_ADRESS_SIRET: '"http://localhost:3000/siret/"',
+  BASE_ADRESS_SIREN: '"http://localhost:3000/siren/"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,7 @@
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+
+  BASE_ADRESS: '"https://sirene.entreprise.api.gouv.fr/full_text/"',
+  BASE_ADRESS_SIRET: '"https://sirene.entreprise.api.gouv.fr/siret/"',
+  BASE_ADRESS_SIREN: '"https://sirene.entreprise.api.gouv.fr/siren/"'
 }
