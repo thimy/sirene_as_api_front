585bcfd2350d06b34869ae0cb6204676ac57e205
Samuel FAURE <samuel.faure.dev@gmail.com>
2017-12-23 08:31:04 +0100

    Adding links on homepage and footer

--- src/components/home/Api.vue
+++ src/components/home/Api.vue
@@ -4,14 +4,14 @@
       <div class="row">
         <div class="column">
           <p>Vous pouvez, si vous le souhaitez, interroger directement l'API Sirene qui est accessible à tous.</p>
-          <a class="button">Voir la documentation</a>
+          <a class="button" href="https://github.com/sgmap/sirene_as_api/blob/master/README.md">Voir la documentation</a>
         </div>
         <div class="column api__image">
           <img src="../../assets/img/man-at-desktop.svg" alt="" width="200" />
         </div>
         <div class="column">
           <p>Notre API est open-source, installez votre propre instance en récupérant les sources sur Github.</p>
-          <a class="button">Voir le code sur Github</a>
+          <a class="button" href="https://github.com/sgmap/sirene_as_api">Voir le code sur Github</a>
         </div>
       </div>
     </div>
