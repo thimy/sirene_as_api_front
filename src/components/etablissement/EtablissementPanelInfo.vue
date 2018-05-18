d4fb29c9206dc2ce94e9bb4e4570760bca96d9d6
Samuel FAURE <samuel.faure.dev@gmail.com>
2018-05-18 09:28:10 +0200

    Fixed Siren/Siret casing, removed a typo

--- src/components/etablissement/EtablissementPanelInfo.vue
+++ src/components/etablissement/EtablissementPanelInfo.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="company__panel panel">
     <ul class="company__info-list">
-      <div class="company__item"><div class="company__item-key">Siret</div><div class="company__item-value"> {{ result.siret }}</div></div>
-      <div class="company__item"><div class="company__item-key">Siren</div><div class="company__item-value"> {{ result.siren }}</div></div>
+      <div class="company__item"><div class="company__item-key">SIRET</div><div class="company__item-value"> {{ result.siret }}</div></div>
+      <div class="company__item"><div class="company__item-key">SIREN</div><div class="company__item-value"> {{ result.siren }}</div></div>
       <div class="company__item"><div class="company__item-key">Clef NIC</div><div class="company__item-value"> {{ result.nic }}</div></div>
       <div class="company__item"><div class="company__item-key">Activité principale</div><div class="company__item-value"> {{ result.activite_principale_entreprise }} - {{ result.libelle_activite_principale_entreprise }}</div></div>
       <div class="company__item"><div class="company__item-key">Nature juridique</div><div class="company__item-value"> {{ result.libelle_nature_juridique_entreprise | ifExist }}</div></div>
