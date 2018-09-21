8c9414b707eab6c921deb0c1aa378eca3d2621cf
Samuelfaure <samuel.faure.dev@gmail.com>
2018-09-21 13:06:50 +0200

    fixed small css issue on RNA pages, temporarily switched sandbox api call to sandbox2 since sandbox SSL doesnt work

--- config/sandbox.env.js
+++ config/sandbox.env.js
@@ -2,13 +2,13 @@ module.exports = {
   NODE_ENV: '"sandbox"',
 
   // WARNING: this will make 'non IP failover website' use the 'IP failover API' !
-  BASE_ADRESS_SIRENE_FULLTEXT: '"https://sandbox.entreprise.data.gouv.fr/api/sirene/v1/full_text/"',
-  BASE_ADRESS_SIRENE_SIRET: '"https://sandbox.entreprise.data.gouv.fr/api/sirene/v1/siret/"',
-  BASE_ADRESS_SIRENE_SIREN: '"https://sandbox.entreprise.data.gouv.fr/api/sirene/v1/siren/"',
-  BASE_ADRESS_SIRENE_SUGGESTIONS: '"https://sandbox.entreprise.data.gouv.fr/api/sirene/v1/suggest/"',
-  BASE_ADRESS_SIRENE_NEAR_ETABLISSEMENT: '"https://sandbox.entreprise.data.gouv.fr/api/sirene/v1/near_etablissement_geojson/"',
-  BASE_ADRESS_SIRENE_ID_ASSOCIATION: '"https://sandbox.entreprise.data.gouv.fr/api/sirene/v1/rna/"',
+  BASE_ADRESS_SIRENE_FULLTEXT: '"https://sandbox2.entreprise.data.gouv.fr/api/sirene/v1/full_text/"',
+  BASE_ADRESS_SIRENE_SIRET: '"https://sandbox2.entreprise.data.gouv.fr/api/sirene/v1/siret/"',
+  BASE_ADRESS_SIRENE_SIREN: '"https://sandbox2.entreprise.data.gouv.fr/api/sirene/v1/siren/"',
+  BASE_ADRESS_SIRENE_SUGGESTIONS: '"https://sandbox2.entreprise.data.gouv.fr/api/sirene/v1/suggest/"',
+  BASE_ADRESS_SIRENE_NEAR_ETABLISSEMENT: '"https://sandbox2.entreprise.data.gouv.fr/api/sirene/v1/near_etablissement_geojson/"',
+  BASE_ADRESS_SIRENE_ID_ASSOCIATION: '"https://sandbox2.entreprise.data.gouv.fr/api/sirene/v1/rna/"',
 
-  BASE_ADRESS_RNA_FULLTEXT: '"https://sandbox.entreprise.data.gouv.fr/api/rna/v1/full_text/"',
-  BASE_ADRESS_RNA_ID_ASSOCIATION: '"https://sandbox.entreprise.data.gouv.fr/api/rna/v1/id/"'
+  BASE_ADRESS_RNA_FULLTEXT: '"https://sandbox2.entreprise.data.gouv.fr/api/rna/v1/full_text/"',
+  BASE_ADRESS_RNA_ID_ASSOCIATION: '"https://sandbox2.entreprise.data.gouv.fr/api/rna/v1/id/"'
 }
