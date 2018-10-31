8318ef9b50713adf9b5d113b1a9c8680486f9e43
Samuelfaure <samuel.faure.dev@gmail.com>
2018-10-31 10:10:01 +0100

    rebasing work

--- src/components/apiDoc/apiDocSirene/ApiDocSireneBetterSearches.vue
+++ src/components/apiDoc/apiDocSirene/ApiDocSireneBetterSearches.vue
@@ -51,7 +51,7 @@
           </table>
           <p>
             Ces options sont disponibles pour le moment uniquement sur la recherche Texte.<br>
-            Notre API est en constante évolution, n'hésitez pas à nous écrire pour demander davantage d'options correspondant à vos besoins !
+            L'API est en constante évolution, n'hésitez pas à nous écrire pour demander davantage d'options correspondant à vos besoins !
           </p>
         </div>
       </div>
