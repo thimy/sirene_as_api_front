326b1cdb534cbabced0b4958ecb8fcbd5ae3ded0
Samuelfaure <samuel.faure.dev@gmail.com>
2018-11-06 16:21:16 +0100

    Changed title of part reprepresentants

--- src/components/etablissement/etablissementRNCS/EtablissementRNCSGestionPhysique.vue
+++ src/components/etablissement/etablissementRNCS/EtablissementRNCSGestionPhysique.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="managersPhysical.length" class="company__panel panel">
-    <h4>Représentants Légaux - Personnes physiques</h4>
+    <h4>Gestion, Direction, Administration, Contrôle, Associés ou Membres - Personnes physiques</h4>
     <div class="company__managers">
       <div v-for="manager in managersPhysical" :key=manager.id>
         <h5>{{ manager.qualite | capitalize }}</h5>
