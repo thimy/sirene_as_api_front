d5c07079b94bcf368b0d2f212b7b4174625eb379
Samuelfaure <samuel.faure.dev@gmail.com>
2018-11-06 19:29:01 +0100

    reverted last commit for aesthetism

--- src/components/etablissement/etablissementRNCS/EtablissementRNCSGestion.vue
+++ src/components/etablissement/etablissementRNCS/EtablissementRNCSGestion.vue
@@ -5,7 +5,7 @@
       <div v-for="manager in managersLegal" :key=manager.id>
         <h5>{{ manager.qualite | ifExist | capitalize }}</h5>
         <panel-info-rncs :parent="manager" :elements=elementsToDisplayLegal1 :inlineLabels="true" />
-        <div class="company__item-inline">
+        <div v-if="manager.siren_pm" class="company__item-inline">
           <div class="company__item-key">SIREN</div>
           <div class="company__item-value">
             <router-link :to="{ name: 'Etablissement', params: {searchId: manager.siren_pm}}"> {{ manager.siren_pm }}</router-link>
@@ -20,23 +20,6 @@
             <div> {{ RNCSConcatAddress(manager) }}</div>
           </div>
         </div>
-        <div v-if="representName(manager)" class="company__item lower">
-          <div class="company__item-key">Représentant Permanent :</div>
-        </div>
-        <div v-if="representName(manager)" class="company__item-inline">
-          <div class="company__item-key">Nom</div>
-          <div class="company__item-value">{{ representName(manager) }}</div>
-        </div>
-        <div v-if="representName(manager) && RNCSConcatAddressRP(manager)" class="company__item-inline">
-          <div class="company__item-key">Adresse</div>
-          <div class="company__item-value">
-            <div>{{ manager.representant_permanent_adresse_ligne_1 | ifExist }}</div>
-            <div v-if="manager.representant_permanent_adresse_ligne_2">{{ manager.representant_permanent_adresse_ligne_2 }}</div>
-            <div v-if="manager.representant_permanent_adresse_ligne_3">{{ manager.representant_permanent_adresse_ligne_3 }}</div>
-            <div> {{ RNCSConcatAddressRP(manager) }}</div>
-          </div>
-        </div>
-        <panel-info-rncs :parent="manager" :elements=elementsToDisplayLegal2 :inlineLabels="true" />
       </div>
       <div v-for="manager in managersPhysical" :key=manager.id>
         <h5>{{ manager.qualite | ifExist | capitalize }}</h5>
