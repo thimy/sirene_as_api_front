c35e4d961fbea385741d01774d4f6231bc6cfd85
Samuelfaure <samuel.faure.dev@gmail.com>
2018-12-06 16:25:31 +0100

    Added opt-out analytics to legal mentions

--- src/components/LegalNotes.vue
+++ src/components/LegalNotes.vue
@@ -2,6 +2,13 @@
   <section class="section section-white center">
     <div class="container">
       <h2>Mentions légales</h2>
+      <h3>Cookies et suivi d'audience</h3>
+      <p>Nous enregistrons des logs anonymes pour suivre l'évolution du site et les pratiques
+        de nos utilisateurs.<br/>
+        Aucune donnée n'est utilisée à des fins commerciales.<br/>
+        Pour en savoir plus ou pour refuser le suivi des sites Data.gouv,
+        <a href="https://beta.gouv.fr/suivi/">cliquez ici.</a>
+      </p>
       <h3>Editeur</h3>
       <p>Mission Etalab, Direction interministérielle du numérique et du système d’information et de communication de l’Etat (DINSIC).</p>
       <h3>Directrice de la publication</h3>
