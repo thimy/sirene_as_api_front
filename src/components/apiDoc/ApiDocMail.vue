b3047620225a5f658cc781d5836447a028519dc6
Samuelfaure <samuel.faure.dev@gmail.com>
2018-12-06 12:08:35 +0100

    Added mailjet warning on mailing list page

--- src/components/apiDoc/ApiDocMail.vue
+++ src/components/apiDoc/ApiDocMail.vue
@@ -1,6 +1,9 @@
 <template>
   <form>
     <h4>Inscription Mailing-List</h4>
+    <p>
+      Nous utilisons Mailjet pour la gestion de vos données.<br/> Vos informations ne seront jamais utilisées à des fins commerciales.
+    </p>
     <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://app.mailjet.com/widget/iframe/zR1/ack" width="340" height="95"></iframe>
   </form>
 </template>
@@ -12,6 +15,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+h4 {
+  margin-bottom: 0;
+}
 form {
   display: flex;
   flex-direction: column;
