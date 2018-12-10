575af55268fd467d07a90af4d2b0127319f08e82
Samuelfaure <samuel.faure.dev@gmail.com>
2018-12-10 14:32:16 +0100

    new wording for cleared offers

--- src/components/apiDoc/ApiDocIntro.vue
+++ src/components/apiDoc/ApiDocIntro.vue
@@ -3,10 +3,13 @@
     <div class="container text-center">
       <h1>Les APIs Open Data</h1>
       <p class="text-center lead-text">
-        Entreprise.data.gouv.fr est en phase intensive de développement.<br/>
-        De nombreuses APIs sont en cours d’ajout !<br/>
+        Entreprise.data.gouv.fr met à votre disposition des APIs open-source gratuites<br/>
+        pour faciliter l'utilisation des données par les professionnels et les particuliers.<br/>
+      </p>
+      <p class="text-center lead-text">
+        De nombreuses APIs sont en cours d’ajout.
         Vous pouvez vous inscrire à notre mailing-list pour vous tenir au courant
-        des nouveautés et changements à venir.
+        des nouveautés et changements à venir.<br />
       </p>
       <api-doc-mail />
     </div>
--- src/components/apiDoc/ApiDocOutro.vue
+++ src/components/apiDoc/ApiDocOutro.vue
@@ -1,15 +1,20 @@
 <template>
   <section class="section section-white">
     <div class="container text-center">
-      <h2>Découvrez l’API Entreprise, toutes les informations sur les sociétés.</h2>
+      <h2>Notre autre produit : l'API Entreprise.</h2>
       <div class="column__img">
         <img src="@/assets/img/city.svg" alt="">
       </div>
       <h3>Vous justifiez d’une mission de service public ? L’API Entreprise est pour vous.</h3>
       <p>
         Dans le respect du principe "Dites le nous une seule fois", la DINSIC a mis en place l’API Entreprise,
-        l’API qui aggrège les informations sur les entreprises.<br>
-        Obtenez les Greffes, Certificats, Liasses fiscales, Attestations sociales, et bien plus encore...
+        l’API qui aggrège les informations sur les entreprises.<br/>
+        Obtenez les Greffes, Certificats, Liasses fiscales, Attestations sociales, et bien plus encore...<br/>
+      </p>
+      <p>
+        <strong>Attention :</strong> Ces données ne sont pas publiques et sont donc reservées
+        aux administrations ou entreprises justifiant d'une mission de service public.<br/>
+        Pour toutes questions : <a href="maito:tech@entreprise.api.gouv.fr">tech@entreprise.api.gouv.fr</a>
       </p>
       <a class="button" href="https://entreprise.api.gouv.fr/">Demander un accès</a>
     </div>
